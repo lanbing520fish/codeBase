dfccc6741ec59fe9e50e1a29907f65a30e1e0171
heyue <289383680@qq.com>
2021-11-16 17:50:25 +0800

    toString

--- toString.js
+++ toString.js
@@ -1,25 +1,24 @@
-var a = {
-    default: 0,
-    toString: function () {
-        if (this.default == 1) {
-            return 12
-        } else {
-            this.default = 1;
-            return 1
-        }
-        // return ++this.default
-    }
-}
-
-
-// var a = 0;
-// Object.defineProperty(window, "a", {
-//     get () {
-//         console.log(this)
-//         return ++_default
+// var a = {
+//     default: 0,
+//     toString: function () {
+//         if (this.default == 1) {
+//             return 12
+//         } else {
+//             this.default = 1;
+//             return 1
+//         }
+//         // return ++this.default
 //     }
-// })
+// }
 
-if (a == 1 && a == 12){
+
+var _default = 0;
+Object.defineProperty(window, "a", {
+    get () {
+        console.log(this)
+        return ++_default
+    }
+})
+if (a === 1 && a === 2){
     console.log('success')
 }
